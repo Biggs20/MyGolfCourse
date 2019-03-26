c833e9b209fb0cfd61e78eca64fe2ae6a4165d2a
biggs <biggs2733@yahoo.com>
2019-03-26 09:06:43 -0500

    Initial Check In/ Creation

--- db/create.sql
+++ db/create.sql
@@ -1,27 +1,152 @@
 
-CREATE TABLE memberships (
-                membershipId INTEGER NOT NULL,
-                membershipName VARCHAR NOT NULL,
-                membershipPrice VARCHAR NOT NULL,
-                CONSTRAINT memberships_pk PRIMARY KEY (membershipId)
+CREATE TABLE courselength (
+                courseId INT NOT NULL,
+                redFrontNineLength INT NOT NULL,
+                redBackNineLength INT NOT NULL,
+                redTotalLength INT NOT NULL,
+                goldFrontNineLength INT NOT NULL,
+                goldBackNineLength INT NOT NULL,
+                goldTotalLength INT NOT NULL,
+                whiteFrontNineLength INT NOT NULL,
+                whiteBackNineLength INT NOT NULL,
+                whiteTotalLength INT NOT NULL,
+                blueFrontNineLength INT NOT NULL,
+                blueBackNineLength INT NOT NULL,
+                blueTotalLength INT NOT NULL,
+                blackFrontNineLength INT NOT NULL,
+                blackBackNineLength INT NOT NULL,
+                blackTotalLength INT NOT NULL,
+                PRIMARY KEY (courseId)
 );
 
 
-CREATE TABLE members (
-                memberId INTEGER NOT NULL,
+CREATE TABLE course (
+                courseId INT NOT NULL,
+                courseName VARCHAR(50) NOT NULL,
+                address VARCHAR(40) NOT NULL,
+                city VARCHAR(20) NOT NULL,
+                stateId CHAR(2) NOT NULL,
+                PRIMARY KEY (courseId)
+);
+
+
+CREATE TABLE product (
+                productId INT AUTO_INCREMENT NOT NULL,
+                productName VARCHAR(30) NOT NULL,
+                price DECIMAL NOT NULL,
+                brand VARCHAR(30) NOT NULL,
+                style VARCHAR(30) NOT NULL,
+                picture LONGBLOB,
+                PRIMARY KEY (productId)
+);
+
+
+CREATE TABLE amenity (
+                amenityId INT NOT NULL,
+                picture LONGBLOB,
+                membershipId INT NOT NULL,
+                amenityName VARCHAR(30) NOT NULL,
+                openHours TIME,
+                PRIMARY KEY (amenityId)
+);
+
+
+CREATE TABLE hole (
+                holeNumber INT NOT NULL,
+                redTeeLength INT NOT NULL,
+                goldTeeLength INT NOT NULL,
+                whiteTeeLength INT NOT NULL,
+                blueTeeLength INT NOT NULL,
+                blackTeeLength INT NOT NULL,
+                courseId INT NOT NULL,
+                par INT NOT NULL,
+                picture LONGBLOB,
+                mensHandicap INT NOT NULL,
+                womensHandicap VARCHAR NOT NULL,
+                PRIMARY KEY (holeNumber)
+);
+
+
+CREATE TABLE membership (
+                membershipId INT AUTO_INCREMENT NOT NULL,
+                membershipName VARCHAR(30) NOT NULL,
+                membershipPrice DECIMAL NOT NULL,
+                PRIMARY KEY (membershipId)
+);
+
+
+CREATE TABLE membershipamenity (
+                membershipAmenityId INT NOT NULL,
+                membershipId INT NOT NULL,
+                amenityId INT NOT NULL,
+                PRIMARY KEY (membershipAmenityId)
+);
+
+
+CREATE TABLE member (
+                memberId INT AUTO_INCREMENT NOT NULL,
                 lastName VARCHAR(20) NOT NULL,
                 firstName VARCHAR(20) NOT NULL,
-                address VARCHAR NOT NULL,
-                phoneNumber VARCHAR NOT NULL,
-                dateJoined VARCHAR NOT NULL,
-                membershipId INTEGER NOT NULL,
-                CONSTRAINT members_pk PRIMARY KEY (memberId)
+                address VARCHAR(40) NOT NULL,
+                city VARCHAR(25) NOT NULL,
+                stateId CHAR(2) NOT NULL,
+                phoneNumber CHAR(10) NOT NULL,
+                dateJoined DATE NOT NULL,
+                membershipId INT NOT NULL,
+                picture LONGBLOB,
+                handicap INT,
+                PRIMARY KEY (memberId)
 );
 
 
-ALTER TABLE members ADD CONSTRAINT memberships_members_fk
-FOREIGN KEY (membershipId)
-REFERENCES memberships (membershipId)
+CREATE TABLE orderdetail (
+                orderNumber INT DEFAULT 100 NOT NULL,
+                productId INT NOT NULL,
+                memberId INT NOT NULL,
+                salePrice DECIMAL NOT NULL,
+                quantity INT NOT NULL,
+                PRIMARY KEY (orderNumber)
+);
+
+
+ALTER TABLE course ADD CONSTRAINT courselength_course_fk
+FOREIGN KEY (courseId)
+REFERENCES courselength (courseId)
 ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
+ON UPDATE NO ACTION;
+
+ALTER TABLE hole ADD CONSTRAINT courses_holes_fk
+FOREIGN KEY (courseId)
+REFERENCES course (courseId)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE orderdetail ADD CONSTRAINT products_orderdetail_fk
+FOREIGN KEY (productId)
+REFERENCES product (productId)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE membershipamenity ADD CONSTRAINT amenity_membershipamenity_fk
+FOREIGN KEY (amenityId)
+REFERENCES amenity (amenityId)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE member ADD CONSTRAINT memberships_members_fk
+FOREIGN KEY (membershipId)
+REFERENCES membership (membershipId)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE membershipamenity ADD CONSTRAINT memberships_membershipamenity_fk
+FOREIGN KEY (membershipId)
+REFERENCES membership (membershipId)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE orderdetail ADD CONSTRAINT members_orderdetail_fk
+FOREIGN KEY (memberId)
+REFERENCES member (memberId)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
---
+++ db/deleteall.sql
@@ -0,0 +1,3 @@
+
+DELETE FROM memberships;
+DELETE FROM members;
