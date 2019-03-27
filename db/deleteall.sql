101c31acf27397dae0b034cb183816583aac9092
biggs <biggs2733@yahoo.com>
2019-03-26 19:49:34 -0500

    Initial Check In/ Creation

--- db/deleteall.sql
+++ db/deleteall.sql
@@ -2,7 +2,7 @@ DELETE FROM amenity;
 DELETE FROM course;
 DELETE FROM courselength;
 DELETE FROM hole;
-DELETE FROM member; 
+DELETE FROM member;
 DELETE FROM membership;
 DELETE FROM orderdetail;
 DELETE FROM product;
