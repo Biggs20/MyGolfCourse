c7787ac925bdcf68ecaa90446a0bc04c89e200d4
biggs <biggs2733@yahoo.com>
2019-03-29 15:45:51 -0500

    Added Map

--- public/javascripts/leaflet.js
+++ public/javascripts/leaflet.js
@@ -1,26 +1,24 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([35.102232, -92.501387],13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibHJmbHJmIiwiYSI6ImNqdG94bWQwdTQ4Mng0Ym11YW52MWZ5bnEifQ.lZEH9-1FD4H2KYhVZDIe4Q', {
-maxZoom: 18,
+maxZoom: 15,
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
   '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
   'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 id: 'mapbox.streets'
 }).addTo(mymap);
 
-L.marker([51.5, -0.09]).addTo(mymap).on('click', onClick);
+L.marker([35.102232, -92.501387]).addTo(mymap).on('click', onClick);
 
-L.circle([51.508, -0.11], {
-color: 'red',
+L.circle([35.102232, -92.501387], {
+color: 'blue',
 fillColor: '#f03',
 fillOpacity: 0.5,
-radius: 1000
+radius: 100
 }).addTo(mymap);
 
 L.polygon([
-[51.509, -0.08],
-[51.503, -0.06],
-[51.51, -0.047]
+[35.102232, -92.501387]
 ]).addTo(mymap);
 
 function onClick(e) {
