f40914f209efdf114c39eca58aa62fb4a6455c0a
biggs <biggs2733@yahoo.com>
2019-04-01 18:16:11 -0500

    Added Temp & Wind to Home Page

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import models.Weather.WeatherResult;
+import play.Logger;
 import play.data.FormFactory;
 import play.db.jpa.JPAApi;
 import play.db.jpa.Transactional;
@@ -7,6 +10,8 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 import javax.inject.Inject;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class HomeController extends Controller
 {
@@ -25,7 +30,38 @@ public class HomeController extends Controller
     @Transactional
     public Result getHome()
     {
-        return ok(views.html.home.render());
+        WeatherResult weatherResult = null;
+
+        //to get weather results
+        try
+        {
+            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=-92&appid=3b58c83e16da201f3aec2c01b8535402&units=Imperial");
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+            Logger.debug("weather api status: " + request.getResponseCode());
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            weatherResult = objectMapper.readValue(url, WeatherResult.class);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            Logger.error(e.getMessage());
+        }
+
+        double temp = weatherResult.getMain().getTemp();
+        String realTemp = "" + temp;
+        String normalTemp = realTemp.substring(0, 2);
+
+        double wind = weatherResult.getWind().getSpeed();
+        String realSpeed = "" + wind;
+        String normalSpeed = realSpeed.substring(0, 2);
+
+        if(realSpeed.length() < 5)
+        {
+            normalSpeed = realSpeed.substring(0, 1);
+        }
+
+        return ok(views.html.home.render(normalTemp, normalSpeed));
     }
 
     @Transactional
@@ -33,4 +69,5 @@ public class HomeController extends Controller
     {
         return ok(views.html.contactinfo.render());
     }
+
 }
