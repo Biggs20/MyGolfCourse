e8e741cf8db70177e1c8aa761da8f5de045f34a1
biggs <biggs2733@yahoo.com>
2019-04-02 17:27:12 -0500

    Added Base Controller

---
+++ app/controllers/BaseController.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import models.Member;
+import play.Logger;
+import play.mvc.Controller;
+
+public class BaseController extends Controller
+{
+    private final String LOGGED_IN_MEMBER = "LoggedInMemberId";
+
+    public void login(Member member)
+    {
+        session().put(LOGGED_IN_MEMBER, "" + member.getMemberId());
+    }
+
+    public void logout()
+    {
+        session().remove(LOGGED_IN_MEMBER);
+    }
+
+    public boolean isLoggedIn()
+    {
+        return session().containsKey(LOGGED_IN_MEMBER);
+    }
+
+    public Integer getLoggedInEmployeeId()
+    {
+        String memberIdText = session().get(LOGGED_IN_MEMBER);
+
+        Integer memberId = null;
+
+        if (memberIdText != null)
+        {
+            memberId = Integer.parseInt(memberIdText);
+        }
+
+        return memberId;
+    }
+}
